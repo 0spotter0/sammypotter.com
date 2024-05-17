1d12946d0dde7c5f2b9003183ad6a5e5ee15eba8
sammypotter <50476978+0spotter0@users.noreply.github.com>
2024-05-17 19:27:18 +0200

    fix 404 page formatting

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,16 +1,16 @@
 'use server'
 
 import Link from 'next/link'
-import { IoHome } from 'react-icons/io5'
+import { IoArrowBack } from 'react-icons/io5'
 
 export default async function NotFound() {
   return (
-    <div className='flex h-svh w-svw flex-col items-center justify-center gap-8'>
-      <h1 className='text-2xl font-medium'>Page not found</h1>
+    <div className='flex flex-col items-center justify-center gap-8 pt-20'>
+      <h1 className='font-mono text-xl font-medium'>Page not found</h1>
       <Link href='/'>
-        <div className='flex gap-2'>
-          <IoHome />
-          <p className='text-xl font-medium'>back</p>
+        <div className='flex items-center gap-2'>
+          <IoArrowBack className='text-xl' />
+          <p className='font-mono text-lg font-medium underline'>back</p>
         </div>
       </Link>
     </div>
