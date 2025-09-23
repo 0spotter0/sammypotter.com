6a84f84d2f25c5056206c2e02be19560034439d0
sammypotter <50476978+0spotter0@users.noreply.github.com>
2025-09-23 01:38:21 -0400

    fix resume title to just name

--- app/resume/download/page.tsx
+++ app/resume/download/page.tsx
@@ -3,5 +3,5 @@
 import { redirect } from 'next/navigation'
 
 export default async function DownloadResume() {
-  return redirect('/Resume_SamuelPotter_2024.pdf')
+  return redirect('/SamuelPotter.pdf')
 }
